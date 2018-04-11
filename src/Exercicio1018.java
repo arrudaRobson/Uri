08c7819f1e5119231609f285fc080dd16732c394
Robson Arruda <robson.arruda@uni9.edu.br>
2018-04-10 22:07:16 -0300

    correção Exercicio1018

--- src/Exercicio1018.java
+++ src/Exercicio1018.java
@@ -11,34 +11,35 @@ public class Exercicio1018 {
 		Scanner entrada = new Scanner(System.in);
 		  
 		N = entrada.nextInt();
-		if(N > 0 && N < 1000000) {
-			cem = N / 100;
-			N -= cem * 100; 
-			
-			cinqta = (N % 100) / 50; 
-			N -= cinqta * 50; 
-			
-			vinte = (N % 50) / 20; 
-			N -= vinte * 20; 
-			
-			dez = (N % 20) / 10; 
-			N -= dez * 10; 
-			
-			cinco = (N % 10) / 5; 
-			N -= cinco * 5; 
-			
-			dois = (N % 5) / 2; 
-			N -= dois * 2; 
-			
-			um = (N % 2) / 1; 
-			
-			System.out.printf("%d nota(s) de R$ 100,00\n", cem); 
-			System.out.printf("%d nota(s) de R$ 50,00\n", cinqta); 
-			System.out.printf("%d nota(s) de R$ 20,00\n", vinte); 
-			System.out.printf("%d nota(s) de R$ 10,00\n",dez); 
-			System.out.printf("%d nota(s) de R$ 5,00\n", cinco); 
-			System.out.printf("%d nota(s) de R$ 2,00\n", dois); 
-			System.out.printf("%d nota(s) de R$ 1,00\n", um);	
-		}
+		System.out.printf("%d\n", N);
+	
+		cem = N / 100;
+		N -= cem * 100; 
+		
+		cinqta = (N % 100) / 50; 
+		N -= cinqta * 50; 
+		
+		vinte = (N % 50) / 20; 
+		N -= vinte * 20; 
+		
+		dez = (N % 20) / 10; 
+		N -= dez * 10; 
+		
+		cinco = (N % 10) / 5; 
+		N -= cinco * 5; 
+		
+		dois = (N % 5) / 2; 
+		N -= dois * 2; 
+		
+		um = (N % 2) / 1; 
+		
+		System.out.printf("%d nota(s) de R$ 100,00\n", cem); 
+		System.out.printf("%d nota(s) de R$ 50,00\n", cinqta); 
+		System.out.printf("%d nota(s) de R$ 20,00\n", vinte); 
+		System.out.printf("%d nota(s) de R$ 10,00\n",dez); 
+		System.out.printf("%d nota(s) de R$ 5,00\n", cinco); 
+		System.out.printf("%d nota(s) de R$ 2,00\n", dois); 
+		System.out.printf("%d nota(s) de R$ 1,00\n", um);	
+		
     }
 }
