1c6a082b770e9f942e1aaa98b867c59b457efa97
Robson Arruda <robson.arruda@uni9.edu.br>
2018-04-20 21:14:27 -0300

    20/04

--- src/Exercicio1041.java
+++ src/Exercicio1041.java
@@ -1,4 +1,35 @@
+import java.io.IOException;
+import java.util.Scanner;
 
-public class Exercicio1041 {
-
+public class Exercicio1041 
+{
+	public static void main(String[] args)throws IOException 
+	{
+		double x, y;
+		
+		Scanner entrada = new Scanner(System.in);
+		String dados[] = entrada.nextLine().split(" ");
+		
+		x = Double.parseDouble(dados[0]);
+		y = Double.parseDouble(dados[1]);
+		
+		if (x == 0.0 && y == 0.0) {
+			System.out.println("Origem");
+		}
+		if (y == 0.0 && x != 0.0) {
+			System.out.println("Eixo X");
+		}
+		if (x == 0.0 && y != 0.0) {
+			System.out.println("Eixo Y");
+		}
+		if (x > 0.0 && y > 0.0) {
+			System.out.println("Q1");
+		} else if (x < 0.0 && y > 0.0) {
+			System.out.println("Q2");
+		} else if (x < 0.0 && y < 0.0) {
+			System.out.println("Q3");
+		} else if (x > 0.0 && y < 0.0) {
+			System.out.println("Q4");
+		}
+	}
 }
